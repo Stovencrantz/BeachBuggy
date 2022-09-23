6780c4f7a1456460013b18976e9f001458a12f75
Knapp <Steven.Knapp@dep.nj.gov>
2022-09-23 07:07:06 -0400

    implementing fetch api to test GET requests from the openWeather api

--- js/app.js
+++ js/app.js
@@ -1,64 +1,113 @@
 const container = document.querySelector(".container");
-const coffees = [
+
+const beaches = [
   {
-    name: "Perspiciatis",
-    image: "images/coffee1.jpg"
+    name: "Atlantic City",
+    lat: "39.35509",
+    lon: "-74.429796",
   },
   {
-    name: "Voluptatem",
-    image: "images/coffee2.jpg"
+    name: "Avalon",
+    lat: "39.093731",
+    lon: "-74.716779",
   },
   {
-    name: "Explicabo",
-    image: "images/coffee3.jpg"
-  },
-  {
-    name: "Rchitecto",
-    image: "images/coffee4.jpg"
-  },
-  {
-    name: " Beatae",
-    image: "images/coffee5.jpg"
-  },
-  {
-    name: " Vitae",
-    image: "images/coffee6.jpg"
-  },
-  {
-    name: "Inventore",
-    image: "images/coffee7.jpg"
-  },
-  {
-    name: "Veritatis",
-    image: "images/coffee8.jpg"
-  },
-  {
-    name: "Accusantium",
-    image: "images/coffee9.jpg"
+    name: "Avon-By-The-Sea",
+    lat: "40.190581",
+    lon: "-74.008856",
   }
 ];
-const showCoffees = () => {
-  let output = "";
-  coffees.forEach(
-    ({ name, image }) =>
-      (output += `
-              <div class="card">
-                <img class="card--avatar" src=${image} />
-                <h1 class="card--title">${name}</h1>
-                <a class="card--link" href="#">Taste</a>
-              </div>
-              `)
-  );
-  container.innerHTML = output;
-};
+// const showCoffees = () => {
+//   let output = "";
+//   coffees.forEach(
+//     ({ name, image }) =>
+//       (output += `
+//               <div class="card">
+//                 <img class="card--avatar" src=${image} />
+//                 <h1 class="card--title">${name}</h1>
+//                 <a class="card--link" href="#">Taste</a>
+//               </div>
+//               `)
+//   );
+//   container.innerHTML = output;
+// };
 
-document.addEventListener("DOMContentLoaded", showCoffees);
+// document.addEventListener("DOMContentLoaded", showCoffees);
 
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
+  window.addEventListener("load", function () {
     navigator.serviceWorker
       .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err));
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
   });
+}
+
+// script for getting users current position
+var x = document.getElementById("location");
+
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+function success(pos) {
+  const crd = pos.coords;
+
+  console.log("Your current position is: ");
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude : ${crd.longitude}`);
+  console.log(`More of less ${crd.accuracy} meters.`);
+
+  let api = "8988ce4587b71b5353869d036e2f9471";
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=${api}`
+  )
+    .then((response) => {
+      // handle the response
+      console.log("response work");
+      console.log(response)
+    })
+    .catch((error) => {
+      console.log("Is broken");
+    });
+}
+
+function getBeaches() {
+  console.log(beaches);
+  
+
+  let api = "8988ce4587b71b5353869d036e2f9471";
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${beaches[0].lat}&lon=${beaches[0].lon}&appid=${api}`
+  )
+    .then((response) => {
+      // handle the response
+      console.log("response work");
+      console.log(response)
+    })
+    .catch((error) => {
+      console.log("Is broken");
+    });
+
+}
+
+
+async function getLocation() {
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+function showPosition(position) {
+  x.innerHTML =
+    "Latitude: " +
+    position.coords.latitude +
+    "<br>Longitude: " +
+    position.coords.longitude;
 }
